a1194d67eb8d77d46012e0ddfe7846f823671f4a
Rodo Rivera <rudy_roddick@msn.com>
2023-11-16 00:31:35 -0600

    fix in seeds

--- seeds/category_seeds.js
+++ seeds/category_seeds.js
@@ -1,23 +1,25 @@
 const { Category } = require('../models');
-
 const categoryData = [
     {
+        category_id: 1,
         category_name: 'Shirts',
     },
     {
+        category_id: 2,
         category_name: 'Shorts',
     },
     {
+        category_id: 3,
         category_name: 'Music',
     },
     {
+        category_id: 4,
         category_name: 'Hats',
     },
     {
+        category_id: 5,
         category_name: 'Shoes',
     },
 ];
-
 const seedCategories = () => Category.bulkCreate(categoryData);
-
 module.exports = seedCategories;
--- seeds/product_tag_seeds.js
+++ seeds/product_tag_seeds.js
@@ -3,15 +3,15 @@ const { ProductTag } = require('../models');
 const productTagData = [
     {
         product_id: 1,
-        tag_id: 6,
+        tag_id: 3,
     },
     {
         product_id: 1,
-        tag_id: 7,
+        tag_id: 4,
     },
     {
         product_id: 1,
-        tag_id: 8,
+        tag_id: 5,
     },
     {
         product_id: 2,
@@ -19,7 +19,7 @@ const productTagData = [
     },
     {
         product_id: 3,
-        tag_id: 1,
+        tag_id: 6,
     },
     {
         product_id: 3,
@@ -35,6 +35,10 @@ const productTagData = [
     },
     {
         product_id: 4,
+        tag_id: 8,
+    },
+    {
+        product_id: 4,
         tag_id: 1,
     },
     {
@@ -42,12 +46,8 @@ const productTagData = [
         tag_id: 2,
     },
     {
-        product_id: 4,
-        tag_id: 8,
-    },
-    {
         product_id: 5,
-        tag_id: 3,
+        tag_id: 5,
     },
 ];
 
